e87ec86a4774eaf69de467c98cdc6aff4df381e8
Alberto <Alberto@MacBook-Pro-de-Alberto-Amigo-Alonso.local>
2014-11-22 00:03:54 +0100

    Faltar arreglar el problema del numerador

--- calculo.asm
+++ calculo.asm
@@ -1,34 +1,55 @@
 
-extern x, y
+extern x, y, a1, a2, a3, a4, a5, a6
 
 global calculo
 
 segment .data
-
 segment .bss
-
+	numerador resq 1
 segment .text
-
 calculo:
-
 	finit
-	fld dword[1]
-	fld dword[x]
+L1:
+	fld1
+	fld qword[x]
+L2:
 	fabs
 	fcomi st1
-	jg Mayor
+	ja Mayor
 
 Menor:
-	mov dword[y], 1
+L3:
+	fld1
+	fst qword[y]
+	ret
 
 Mayor:
+	finit
 	fldl2e
+	fld qword[x]
 	fmul st1, st0
+
+	fst qword[a1]
+
 	fsin
-	fscale
+
+	fst qword[a2]
 	
+	fscale
 
+	fst qword[a3]
 
+	fst qword[numerador]
+	
+	finit
+	fld1
+	fld qword[x]
+	fld qword[x]
+	fmulp st1
+	fsub st1
+	fsqrt
+	fld qword[numerador]
+	fdiv st1
 
-
-
+	fst qword[y]
+	ret
--- p2.c
+++ p2.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
-double x, y;
+double x, y, a1, a2, a3, a4, a5, a6;
 double calculo();
 
 void main(){
-	printf("Introduce un valor de x\n");
-	scanf("%f", &x);
-	y = calculo();
-	printf("El valor de y es: %f", y);
+	printf("Introduce un valor de x: ");
+	scanf("%lf", &x);
+	calculo();
+	printf("\nEl valor de y es: %lf\n", y);
+	printf("\nEl valor de a1 es: %lf\n", a1);
+	printf("\nEl valor de a2 es: %lf\n", a2);
+	printf("\nEl valor de a3 es: %lf\n", a3);
+	printf("\nEl valor de a4 es: %lf\n", a4);
+	printf("\nEl valor de a5 es: %lf\n", a5);
 }
