e1cbe64118bd94019ae9da8ff1ee98bc734214a8
Alberto <Alberto@MacBook-Pro-de-Alberto-Amigo-Alonso.local>
2014-11-28 21:06:28 +0100

    Entregado con arreglos

--- calculo.asm
+++ calculo.asm
@@ -1,55 +1,62 @@
+; Desarrollado por Sergio Delgado y Alberto Amigo
+; 
+; En este codigo hacemos un uso eficiente de la pila de FPU, habiendo conseguido eliminar
+;	cargas y lecturas de memoria que, siendo evitables, solo harían tardar mas al programa
 
-extern x, y, a1, a2, a3, a4, a5, a6
-
-global calculo
-
-segment .data
 segment .bss
-	numerador resq 1
+	control resw 1
+
 segment .text
-calculo:
-	finit
-L1:
-	fld1
-	fld qword[x]
-L2:
-	fabs
-	fcomi st1
-	ja Mayor
 
-Menor:
-L3:
-	fld1
-	fst qword[y]
-	ret
+	extern x, y
+	global calculo	
 
-Mayor:
-	finit
-	fldl2e
-	fld qword[x]
-	fmul st1, st0
+	calculo:
+		finit
+		fld1					; st0 = 1
+		fld qword[x]
+		fabs					; st0 = |x|, st1 = 1
+		fcomi st1
+		ja Mayor				; Si |x| > 1, entonces f(x) = (sen(x)*e^x)/(raiz2(x^2-1))
+								; 	calculado en la etiqueta Mayor
+	Menor:						; Sino f(x) = 1
+		fld1					; 	calculado en la etiqueta Menor 
+		fst qword[y]
+		jmp Return
 
-	fst qword[a1]
+	Mayor:
+		finit
 
-	fsin
+		fstcw word[control]			; Fijamos los bits de la palabra de control de la FPU
+		or word[control], 0x0C00	; 	para el redondeo
+		fldcw word[control]
 
-	fst qword[a2]
-	
-	fscale
+		fld1						; Será usado en (*) para almacenar al final de la pila un resultado y no perderlo
 
-	fst qword[a3]
-
-	fst qword[numerador]
-	
-	finit
-	fld1
-	fld qword[x]
-	fld qword[x]
-	fmulp st1
-	fsub st1
-	fsqrt
-	fld qword[numerador]
-	fdiv st1
-
-	fst qword[y]
-	ret
+		fldl2e						; Aplicando logaritmos deducimos: e^x = 2^(x*log2(e))
+		fld qword[x]
+		fmulp					
+		fld qword[x]
+		fsin						; Como fscale redondea st1, trabajamos por separado
+		fscale						; 	con parte entera y decimal
+		fmulp st2, st0				; (*) sen(x)*2^redondeo(x*log2(e))
+									; Estado de la pila: (st0)x*log2(e), (st1)sen(x)*2^redondeo(x*log2(e))
+		fld1
+		fmul st1					; Copia de x*log2(e) para no perder su valor al redondear
+		frndint
+		fsubp						; Conseguimos la parte decimal con x*log2(e) - redondeo(x*log2(e))
+		f2xm1
+		fld1						
+		faddp						; 2^parteDecimal + 1 - 1 = 2^parteDecimal
+		fmulp						; sen(x)*2^redondeo(x*log2(e))*2^parteDecimal(x*log2(e))
+									; Estado de la pila: (st0)numerador
+		fld qword[x]
+		fld qword[x]
+		fmulp
+		fld1
+		fsubp
+		fsqrt						; raiz2(x^2-1)
+		fdivp						; Estado de la pila: (st0)numerador/denominador
+		fstp qword[y]
+	Return:
+		ret
--- p2.c
+++ p2.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
-double x, y, a1, a2, a3, a4, a5, a6;
+double x, y;
 double calculo();
 
 void main(){
 	printf("Introduce un valor de x: ");
 	scanf("%lf", &x);
 	calculo();
-	printf("\nEl valor de y es: %lf\n", y);
-	printf("\nEl valor de a1 es: %lf\n", a1);
-	printf("\nEl valor de a2 es: %lf\n", a2);
-	printf("\nEl valor de a3 es: %lf\n", a3);
-	printf("\nEl valor de a4 es: %lf\n", a4);
-	printf("\nEl valor de a5 es: %lf\n", a5);
+	printf("El valor de y es: %lf\n", y);
 }
